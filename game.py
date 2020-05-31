adc46f1d32e6c48324c7dc9b19a3263dd75cdaec
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-05-31 16:12:49 +0200

    quick recap on functions

--- game.py
+++ game.py
@@ -1,41 +1,42 @@
-
 """
-Initiating the game over status of the game
+Initializing the player's position
 """
-is_game_over: bool = False
-
-p_0_x_pos: int = 0
-e_0_x_pos: int = 3
-e_1_x_pos: int = 5
-
-p_0_x_pos += 2
-if p_0_x_pos in (e_0_x_pos, e_1_x_pos):
-    is_game_over: bool = True
-else:
-    e_0_x_pos += 1
-    e_1_x_pos += 1
-
-p_x_pos: int = 2
-e_x_pos: int = 3
-end_x_pos: int = 10
+x_pos = 0
+e_x_pos = 4
 
 
-while not is_game_over:
-    print('player position' + str(p_x_pos))
-    print('enemy position' + str(e_x_pos))
-    if p_x_pos == e_x_pos:
-        print('You lose')
-        is_game_over = True
-    elif p_x_pos >= end_x_pos:
-        is_game_over = True
-        print('You win')
+def move():
+    """
+    move
+    """
+    global x_pos
+    x_pos += 1
+
+
+move()
+
+
+def move_by(amount):
+    """
+    move by amount
+    """
+    global x_pos
+    x_pos += amount
+
+
+def check_for_collision():
+    """
+    Check for the collision between enemy and player
+    :return: True if collision happened
+    """
+    global x_pos
+    global e_x_pos
+    if x_pos == e_x_pos:
+        return True
     else:
-        p_x_pos += 3
-        e_x_pos += 1
+        return False
 
-x_pos = 5
-movements = [1, -2, 6, -3, -2, 4]
 
-for movement in movements:
-    x_pos += movement
-    print(x_pos)
+move_by(8)
+did_collide = check_for_collision()
+print(did_collide)
