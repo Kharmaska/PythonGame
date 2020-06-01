bd138d3347e52051ff1f0c7cb5ebec24bb9b8ef6
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-06-01 11:23:54 +0200

    Player's character up and running, properly moving up and down and redrawing the background correctly

--- python_game.py
+++ python_game.py
@@ -19,6 +19,9 @@ clock = pygame.time.Clock()
 
 
 class Game:
+    """
+    Main loop of the game
+    """
     TICK_RATE = 60
 
     # Initializer for the game class to set up width, height and title
@@ -38,6 +41,9 @@ class Game:
         Main loop for the game to draw graphics per tick rate and also to quit game once game is over
         """
         is_game_over = False
+        direction = 0
+
+        player_character = PlayerCharacter('player.png', 375, 700, 50, 50)
 
         # Main game loop, used to update all gameplay such as movements, checks and graphics
         # Runs until is_game_over = True
@@ -48,14 +54,19 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_game_over = True
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
                 print(event)
 
-            # Draws a rectangle on top of the game screen canvas (x, y, width, height)
-            # pygame.draw.rect(game_screen, BLACK_COLOR, [350, 350, 100, 100])
-            # Draws a circle on top of the game screen canvas (x, y, radius)
-            # pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
-
-            # game_screen.blit(player_image, (375, 375))
+            self.game_screen.fill(WHITE_COLOR)
+            player_character.move(direction)
+            player_character.draw(self.game_screen)
 
             # Update all game graphics
             pygame.display.update()
@@ -72,8 +83,12 @@ class GameObject:
         object_image = pygame.image.load(image_path)
         # Scales the image up
         self.image = pygame.transform.scale(object_image, (width, height))
+
         self.x_pos = x
         self.y_pos = y
+
+        self.width = width
+        self.height = height
 
     def draw(self, background):
         """
@@ -81,6 +96,26 @@ class GameObject:
         :param background:
         """
         background.blit(self.image, (self.x_pos, self.y_pos))
+
+
+class PlayerCharacter(GameObject):
+    """
+    Draws the player's character
+    """
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        """
+        moves the player's character along the Y axis only
+        :return:
+        """
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 
 pygame.init()
